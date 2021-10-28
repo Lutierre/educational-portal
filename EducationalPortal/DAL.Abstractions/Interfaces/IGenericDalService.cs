5c381c13bc98bc7cb034583dcc8184debad69977
DESKTOP-IHC9G7G\Alice Lutierre <alice.lutierre@gmail.com>
2021-10-29 02:21:19 +0300

    Add Filter, AddMany, DeleteMany methods to GenericDalService

--- EducationalPortal/DAL.Abstractions/Interfaces/IGenericDalService.cs
+++ EducationalPortal/DAL.Abstractions/Interfaces/IGenericDalService.cs
@@ -1,4 +1,6 @@
-﻿using DTO.Models;
+﻿using System;
+using System.Collections.Generic;
+using DTO.Models;
 
 namespace DAL.Abstractions.Interfaces
 {
@@ -7,13 +9,19 @@ namespace DAL.Abstractions.Interfaces
         void Deserialize();
 
         void Serialize();
-        
+
         void Add(T t);
 
+        void AddMany(List<T> entries);
+
+        List<T> Filter(Func<T, bool> criteriaFunc);
+        
         T Get(int id);
 
         void Update(T user);
 
         void Delete(int id);
+
+        void DeleteMany(Func<T, bool> criteriaFunc);
     }
 }
--- EducationalPortal/DAL/Services/GenericDalService.cs
+++ EducationalPortal/DAL/Services/GenericDalService.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using DAL.Abstractions.Interfaces;
 using DTO.Models;
 using Newtonsoft.Json;
@@ -29,7 +31,7 @@ namespace DAL.Services
             var serializer = new JsonSerializer();
             var path = $"Json/{typeof(T).Name}.json";
             
-            using (StreamWriter sw = new StreamWriter(path))
+            using (var sw = new StreamWriter(path))
             using (JsonWriter writer = new JsonTextWriter(sw))
             {
                 serializer.Serialize(writer, _entries);
@@ -38,17 +40,41 @@ namespace DAL.Services
             _entries.Clear();
         }
 
-        public void Add(T t) // add element to collection
+        public void Add(T t)
         {
             Deserialize();
+            t.Id = _entries[^1].Id + 1;
             _entries.Add(t.Id, t);
             Serialize();
         }
 
-        public T Get(int id) // get element from collection
+        public void AddMany(List<T> entries)
         {
             Deserialize();
-            T entryDto = _entries[id];
+            var newId = _entries[^1].Id + 1;
+            
+            foreach (var entry in entries)
+            {
+                entry.Id = newId++;
+                _entries.Add(entry.Id, entry);
+            }
+            
+            Serialize();
+        }
+
+        public List<T> Filter(Func<T, bool> criteriaFunc)
+        {
+            Deserialize();
+            var filteredEntries = _entries.Values.Where(criteriaFunc).ToList();
+            _entries.Clear();
+            
+            return filteredEntries;
+        }
+
+        public T Get(int id) 
+        {
+            Deserialize();
+            var entryDto = _entries[id];
             _entries.Clear();
             
             return entryDto;
@@ -65,6 +91,18 @@ namespace DAL.Services
         {
             Deserialize();
             _entries.Remove(id);
+            Serialize();
+        }
+
+        public void DeleteMany(Func<T, bool> criteriaFunc)
+        {
+            Deserialize();
+            
+            foreach (var dto in _entries.Values.Where(criteriaFunc))
+            {
+                _entries.Remove(dto.Id);
+            }
+            
             Serialize();
         }
     }
