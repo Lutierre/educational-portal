7bfae388ba4294bc7016bb6b0723e6ac21126764
DESKTOP-IHC9G7G\Alice Lutierre <alice.lutierre@gmail.com>
2021-10-30 13:57:34 +0300

    Implement MaterialService

--- EducationalPortal/BLL/Services/MaterialService.cs
+++ EducationalPortal/BLL/Services/MaterialService.cs
@@ -1,59 +1,129 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using BLL.Abstractions.Interfaces;
+using Core.Models;
 using Core.Models.Materials;
+using DAL.Abstractions.Interfaces;
 
 namespace BLL.Services
 {
     public class MaterialService : IMaterialService
     {
+        private readonly IEntityDalService<Article> _articleDalService;
+        private readonly IEntityDalService<Book> _bookDalService;
+        private readonly IEntityDalService<Video> _videoDalService;
+        private readonly ICurrentStateService _currentStateService;
+        private readonly IEntityDalService<Course> _courseDalService;
+        private readonly IEntityDalService<User> _userDalService;
+
+        public MaterialService(IEntityDalService<Article> articleDalService, 
+            ICurrentStateService currentStateService, 
+            IEntityDalService<Course> courseDalService,
+            IEntityDalService<Book> bookDalService,
+            IEntityDalService<Video> videoDalService,
+            IEntityDalService<User> userDalService)
+        {
+            _articleDalService = articleDalService;
+            _currentStateService = currentStateService;
+            _courseDalService = courseDalService;
+            _bookDalService = bookDalService;
+            _videoDalService = videoDalService;
+            _userDalService = userDalService;
+        }
+
+        private void AddMaterial(Material material)
+        {
+            var currentCourse = _currentStateService.CurrentCourse;
+            currentCourse.Materials.Add(material);
+            _courseDalService.Update(currentCourse);
+        }
+        
         public void AddNewArticle(string title, string date, string source)
         {
-            throw new System.NotImplementedException();
+           var article = _articleDalService.Add(new Article
+           {
+               Title = title,
+               Date = date,
+               Source = source
+           });
+           
+           AddMaterial(article);
         }
 
         public void AddExistingArticle(int id)
         {
-            throw new System.NotImplementedException();
+            var article = _articleDalService.Get(id);
+            AddMaterial(article);
         }
 
         public void AddNewBook(string title, string authors, int pageCount, string format, int year)
         {
-            throw new System.NotImplementedException();
+            var book = _bookDalService.Add(new Book
+            {
+                Title = title,
+                Authors = authors, 
+                PageCount = pageCount,
+                Year =  year
+            });
+            
+            AddMaterial(book);
         }
 
         public void AddExistingBook(int id)
         {
-            throw new System.NotImplementedException();
+            var book = _bookDalService.Get(id);
+            AddMaterial(book);
         }
 
         public void AddNewVideo(string title, string duration, string quality)
         {
-            throw new System.NotImplementedException();
+            var video = _videoDalService.Add(new Video { 
+                Title = title, 
+                Duration = duration,
+                Quality = quality 
+            });
+            
+            AddMaterial(video);
         }
 
         public void AddExistingVideo(int id)
         {
-            throw new System.NotImplementedException();
+            var video = _videoDalService.Get(id);
+            AddMaterial(video);
         }
 
         public List<Article> GetArticles()
         {
-            throw new System.NotImplementedException();
+            var result = _currentStateService.CurrentCourse.Materials
+                .Where(material => material.Type == "Article")
+                .Select(material => material as Article).ToList();
+
+            return result;
         }
 
         public List<Book> GetBooks()
         {
-            throw new System.NotImplementedException();
+            var result = _currentStateService.CurrentCourse.Materials
+                .Where(material => material.Type == "Book")
+                .Select(material => material as Book).ToList();
+
+            return result;
         }
 
         public List<Video> GetVideos()
         {
-            throw new System.NotImplementedException();
+            var result = _currentStateService.CurrentCourse.Materials
+                .Where(material => material.Type == "Video")
+                .Select(material => material as Video).ToList();
+
+            return result;
         }
 
         public void LearnMaterial()
         {
-            throw new System.NotImplementedException();
+            var currentUser = _currentStateService.AuthorizedUser;
+            currentUser.Materials.Add(_currentStateService.CurrentMaterial);
+            _userDalService.Update(currentUser);
         }
     }
 }
